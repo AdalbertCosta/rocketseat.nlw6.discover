8ca4dee8d7f2b084c91e9837c16e68d6d31f012c
AdalbertCosta <contato@adalbertocosta.com>
2022-04-24 09:20:31 -0300

    Aula04 - Persistindo os dados

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,7 @@ import Modal from './modal.js'
 const modal = Modal()
 
 //Pegar todos os botoões com a classe check
-const checkButtons = document.querySelectorAll('.actions a.check')
+const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
   //adicionar a escuta
